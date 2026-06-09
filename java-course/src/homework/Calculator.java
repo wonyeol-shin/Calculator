0d58f9099ee202c73dfea36c516b4882560c7201
wonyeolShin <lwy171400@gmail.com>
2026-06-09 10:34:59 +0900

    step2 과제 완료
    클래스 메서드와 클래스 변수(컬렉션)으로 생성
    더하기, 빼기, 곱하기, 나누기 기본 메서드 추가
    수정, 삭제 시 빈 리스트인지 확인 하는 메서드 추가
    수정 시 수정하려는 값이 리스트에 있는지 확인하는 메서드 추가
    조회, 삭제 메서드 추가

--- java-course/src/homework/Calculator.java
+++ java-course/src/homework/Calculator.java
@@ -1,26 +1,99 @@
 package homework;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 abstract public class Calculator {
 
-    static int plus(int num1, int num2) {
-        return num1 + num2;
+    private static Scanner scanner = new Scanner(System.in);
+
+    private static ArrayList<Integer> results = new ArrayList<>();
+
+    // 더하기
+    public static void plus(int num1, int num2) {
+        addResult(num1 + num2);
     }
 
-    static int minus(int num1, int num2) {
-        return num1 - num2;
+    // 빼기
+    public static void minus(int num1, int num2) {
+        addResult(num1 - num2);
     }
 
-    static int multiple(int num1, int num2) {
-        return num1 * num2;
+    // 곱하기
+    public static void multiple(int num1, int num2) {
+        addResult(num1 * num2);
     }
 
-    static double divide(int num1, int num2) {
+    // 나누기
+    public static void divide(int num1, int num2) {
         if (num2 == 0) {
+            System.out.println("연산 취소.");
             System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다");
-            System.out.println("1로 수정하여 다시 계산합니다.");
-            num2 = 1;
+            return;
         }
-        return (double) num1 / num2;
+        addResult(num1 / num2);
     }
+
+    // 계산 결과 저장
+    private static void addResult(int result) {
+        System.out.println("결과 : " + result);
+        results.add(result);
+    }
+
+    // 계산 결과  조회
+    public static void getter() {
+        System.out.println("현재 값 :" + results);
+    }
+
+    // 계산 결과 수정
+    public static void setter() {
+        // 수정할 값 없으면 종료
+        if (checkEmpty()) {
+            System.out.println("수정 할 값이 없습니다.");
+            return;
+        }
+
+        int choice = 0;
+        int changeNum = 0;
+        getter();
+        System.out.print("무슨 값을 변경허시겠습니까?(값 중복 시 왼쪽에 있는 값 우선적용) : ");
+        choice = scanner.nextInt();
+        // 변경 할 값이 있는지 체크
+        if (findIndex(choice)) {
+            System.out.print("변경 할 값 입력");
+            changeNum = scanner.nextInt();
+            results.set(results.indexOf(choice), changeNum);
+            System.out.println("값 수정 완료");
+        }
+    }
+
+    // 계산 결과 삭제(0번 인덱스)
+    public static void deleteFirstResult() {
+        if (checkEmpty()) {
+            System.out.println("삭제 할 값이 없습니다.");
+            return;
+        }
+        System.out.println(results.get(0) + " 삭제 완료");
+        results.remove(results.get(0));
+    }
+
+    // 수정하려는 값이 리스트에 있는지 확인
+    private static boolean findIndex(int choice ) {
+        // 결과가 없다면
+        if (!results.contains(choice)) {
+            return false;
+        }
+        return true;
+    }
+
+    // 삭제 수정 시 리스트가 비어있는지 확인
+    private static boolean checkEmpty() {
+        if (results.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+
+
 
 }
--- java-course/src/homework/CalculatorMain.java
+++ java-course/src/homework/CalculatorMain.java
@@ -2,77 +2,80 @@ package homework;
 
 import java.util.Scanner;
 
+import static homework.Calculator.*;
+
 public class CalculatorMain {
     public static void main(String[] args) {
         int num1 =0;
         int num2 =0;
-        double result = 0;
+
         // 사칙연산 기호 저장
         String bao = "";
         String choice = "";
         Scanner scanner = new Scanner(System.in);
 
         // exit를 받을때까지 동작
-        while ( !choice.equals("exit")) {
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            num1 = scanner.nextInt();
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            num2 = scanner.nextInt();
-            scanner.nextLine();
-            System.out.print("사칙연산 기호를 입력하세요: ");
-            bao = scanner.nextLine();
+        while ( true ) {
+            System.out.println("1.계산하기 2.값 조회 3.값 수정 4.값 삭제 (exit 입력 시 종료)");
 
+            System.out.print("입력 :");
+            choice = scanner.nextLine();
 
-            switch (bao) {
-                case "+" -> {
-                    result = plus(num1, num2);
+            // 계산하기
+            if (choice.equals("1")) {
+                System.out.print("첫 번째 숫자를 입력하세요 : ");
+                num1 = scanner.nextInt();
+                System.out.print("두 번째 숫자를 입력하세요 : ");
+                num2 = scanner.nextInt();
+                scanner.nextLine();
+                System.out.print("사칙연산 기호를 입력하세요 : ");
+                bao = scanner.nextLine();
+
+                switch (bao) {
+                    case "+" -> {
+                        plus(num1, num2);
+                    }
+
+                    case "-" -> {
+                        minus(num1, num2);
+                    }
+
+                    case "*" -> {
+                        multiple(num1, num2);
+                    }
+
+                    case "/" -> {
+                        divide(num1, num2);
+                    }
+
+                    default -> {
+                        System.out.println("사칙연산 기호는 (+, -, *, / )만 사용하세요 ");
+                    }
                 }
-
-                case "-" -> {
-                    result = minus(num1, num2);
-                }
-
-                case "*" -> {
-                    result = multiple(num1, num2);
-                }
-
-                case "/" -> {
-                    result = divide(num1, num2);
-                }
-
-                default -> {
-                    System.out.println("사칙연산 기호는 (+, -, *, / )만 사용하세요 ");
-                    result = 0;
-                }
-
+            }
+            // 값 조회
+            else if (choice.equals("2")) {
+                getter();
+            }
+            // 값 수정
+            else if (choice.equals("3")) {
+                setter();
+            }
+            // 값 삭제
+            else if (choice.equals("4")) {
+                deleteFirstResult();
+            }
+            // 종료
+            else if (choice.equals("exit")) {
+                System.out.println("종료");
+                break;
+            }
+            else {
+                System.out.println("입력 오류");
             }
 
-            System.out.println("연산결과 : " + result);
-
-            System.out.print("더 계산하시겠습니까? (exit 입력 시 종료)");
-            choice = scanner.nextLine();
         }
 
     }
 
-    static int plus(int num1, int num2) {
-        return num1 + num2;
-    }
-
-    static int minus(int num1, int num2) {
-        return num1 - num2;
-    }
-
-    static int multiple(int num1, int num2) {
-        return num1 * num2;
-    }
-
-    static double divide(int num1, int num2) {
-        if (num2 == 0) {
-            System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다");
-            System.out.println("1로 수정하여 다시 계산합니다.");
-            num2 = 1;
-        }
-        return (double) num1 / num2;
-    }
 }
